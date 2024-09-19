8d762b6bc94255dcafa59775bd9b64d1e17ed2cf
jathurwa <115372017+jathurwa@users.noreply.github.com>
2024-09-19 15:36:18 +0300

    hospital_db

---
+++ hospital_db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE hospital_db;
+USE hospital_db;
+CREATE TABLE patients(
+    patient_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    gender VARCHAR(10),
+    language VARCHAR(20) NOT NULL
+);
